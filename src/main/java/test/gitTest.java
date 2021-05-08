334276159296d1f1042fbfdd2e40c27aeb522662
liuchang <ctochang@qq.com>
2021-05-08 19:25:02 +0800

    second commit

--- src/main/java/test/gitTest.java
+++ src/main/java/test/gitTest.java
@@ -1,8 +1,12 @@
 package test;
 
+/**
+ * @author 刘畅
+ */
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 
 }
